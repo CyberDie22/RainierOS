9f3219977c62e0342cacdea150914450f7726a1d
Ben Buzard <ben@benbuzard.com>
2025-07-22 16:54:30 -0700

    Fail cargo build if nasm fails
    Prints output from nasm if it returns an error status, and exits the build with status code 1.

--- build.rs
+++ build.rs
@@ -9,8 +9,19 @@ use std::process::Command;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     // Build stage1 bootloader
-    Command::new("nasm").args(&["src/stage1.asm", "-f", "bin", "-o", &format!("{}/bootloader-stage1.bin", out_dir)])
-        .status().unwrap();
+    let output = Command::new("nasm").args(&["src/stage1.asm", "-f", "bin", "-o", &format!("{}/bootloader-stage1.bin", out_dir)])
+        .output().expect("Failed to execute nasm, is it installed?");
+
+    if !output.status.success() {
+        eprintln!("Failed to assemble stage1.asm with nasm (exit code: {})", output.status);
+
+        if !output.stderr.is_empty() {
+            eprintln!("{}", String::from_utf8_lossy(&output.stderr));
+        }
+
+        std::process::exit(1);
+    }
+
     println!("cargo:rerun-if-changed=src/stage1.asm");
 
     fs::write(".last-compile-dir", out_dir).unwrap();
