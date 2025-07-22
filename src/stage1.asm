3380db22575c2a1801cfdc54a1a63884c1b0f850
Ben Buzard <ben@benbuzard.com>
2025-07-22 16:54:46 -0700

    Print "BIOS Works!" from bootloader.

--- src/stage1.asm
+++ src/stage1.asm
@@ -7,8 +7,33 @@ ORG 0x7C00
 BITS 16
 
 stage1:
+    mov ah, 0xE
+    mov al, 66
+    mov bh, 0x0
+    int 0x10
+    mov al, 73
+    int 0x10
+    mov al, 79
+    int 0x10
+    mov al, 83
+    int 0x10
+    mov al, 32
+    int 0x10
+    mov al, 87
+    int 0x10
+    mov al, 111
+    int 0x10
+    mov al, 114
+    int 0x10
+    mov al, 107
+    int 0x10
+    mov al, 115
+    int 0x10
+    mov al, 33
+    int 0x10
+.halt:
     hlt
-    jmp stage1
+    jmp .halt
 
 times 510-($-$$) db 0
 dw 0xAA55
